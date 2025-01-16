0c40bb1129d93e9cff9f016cdcb0b2ec700e4f19
Jhonny <jhonnyanthony47@gmail.com>
2025-01-16 10:59:50 -0300

    clean: Cleaning the code and add comments

--- src/config.py
+++ src/config.py
@@ -1,17 +1,19 @@
 import os 
-from datetime import datetime
 from dotenv import load_dotenv
+
 dotenv_path = os.path.join(os.path.dirname(__file__), '..', 'venv', '.env')
 load_dotenv(dotenv_path)
 
-email_group = ['jhonny.souza@fgmdentalgroup.com']
-date = datetime.now()
-host = os.getenv("HOST")
+#Database
+host_data = os.getenv('host')
+port_data = os.getenv('port')
+service_name_data=os.getenv('service_name')
+user_data=os.getenv('user')
+password_data=os.getenv('password')
+
+#API
 scope = os.getenv("SCOPE")
-today = date.strftime("%d/%m/%Y")
 tenant_id = os.getenv("TENANT_ID")
 client_id = os.getenv("CLIENT_ID")
 email_from = os.getenv("USER_MAIL")
-username_guest = os.getenv("USER_GUEST")
 client_secret = os.getenv("CLIENT_SECRET")
-password_guest = os.getenv("PASSWORD_GUEST")
