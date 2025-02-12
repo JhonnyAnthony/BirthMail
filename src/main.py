c0ee23b1c338ec59da06f2d2d1a10b75e1d76eff
Jhonny Anthony de Souza <jhonnyanthony47@gmail.com>
2025-02-12 13:44:02 -0300

    refactor: some refactors at logs, now get the ipv4 from starter

--- src/main.py
+++ src/main.py
@@ -4,28 +4,37 @@ from datetime import datetime
 from connectionDB import Database
 from sendMail import SendMail
 from getManager import Manager
+import socket
+import getpass
 class BithMail:
     def __init__(self):
         self.db = Database()
         self.db.connectData()
 
     def logs():
+        nome_host = socket.gethostname()
+        ip_local = socket.gethostbyname(nome_host)
         #log_directory = r"/home/fgm/Scripts/BirthMail/Logs" #path para ser colocado as Logs
         diretorioLocal = os.getcwd()
         log_directory = f"{diretorioLocal}/Logs" #path para ser colocado as Logs
-
+        
+        
         if not os.path.exists(log_directory):
             os.makedirs(log_directory) #caso o path nao exista ele vai criar
 
         current_datetime = datetime.now() #data de hoje
         log_filename = os.path.join(log_directory, current_datetime.strftime("%Y-%m-%d") + "_log.log") #declara o nome do arquivo log
-
         logging.basicConfig(
             level=logging.INFO,
             format="%(asctime)s %(levelname)s %(message)s",
+            encoding='utf-8',
             datefmt="%Y-%m-%d %H:%M:%S",
             filename=log_filename
         )
+        logging.info(f"--------------Informações do executor--------------")
+        logging.info(f"IPV4: {ip_local}")
+        logging.info(f"HOST: {nome_host}") 
+
     logs()
     
 if __name__ == "__main__":
@@ -34,3 +43,4 @@ if __name__ == "__main__":
     manager = Manager()
     manager.connectionDB()
     manager.birthMonth()
+    
